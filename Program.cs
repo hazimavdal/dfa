0f407d31dc9e97c4b2ac5295d83785211d9b0599
Hazim Avdal <hazim@uchicago.edu>
2020-01-11 15:15:11 -0600

    removed debugging code

--- Program.cs
+++ Program.cs
@@ -4,11 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        var dfa = Examples.DivisibleByThree;
-        var word = 9009231.GetBits();
-
-        Console.WriteLine("L(A) Acceptance: {0}", dfa.Read(word).ToString());
-
         Tests.TestDivisibleByThree();
         Tests.TestEvenParity();
         Tests.TestOddMax();
